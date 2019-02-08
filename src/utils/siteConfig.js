075a4c1328d923ce40dc933d5b19f9a2e00af2c0
danklut <aomcloudenterprise@outlook.com>
2019-02-07 22:48:19 -0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -4,7 +4,7 @@ module.exports = {
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
     siteTitleMeta: `AOM Cloud Enterprise Blog`, // This allows an alternative site description for meta data for pages.                                                                                                   
-    siteDescriptionMeta: `A cloud enterprise blog platform with tutorials,tips & tricks,and seo articles to help you manage or build your own cloud enterprise website'.  to build amazing static wehost and Gatbsy`, // This allows an alternative site description for meta data for pages.
+    siteDescriptionMeta: `A cloud enterprise blog platform with tutorials,tips & tricks,and seo articles to help you manage or build your own cloud enterprise website'. // This allows an alternative site description for meta data for pages.
 
     shareImage: `/images/banner.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
     shareImageWidth: 1000, // Change to the width of your default share image
