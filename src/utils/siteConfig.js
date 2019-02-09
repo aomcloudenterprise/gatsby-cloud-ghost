d05bacdce0c12bd7b31ba1618d2e1d3605b071f3
danklut <aomcloudenterprise@outlook.com>
2019-02-09 01:19:31 -0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -1,20 +1,19 @@
 module.exports = {
-    siteUrl: `https://aomcloudenterprise.com', // Site domain. Do not include a trailing slash!
+    siteUrl: 'https://aomcloudenterprise.com', // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `AOM Cloud Enterprise Blog`, // This allows an alternative site description for meta data for pages.                                                                                                   
-    siteDescriptionMeta: `A cloud enterprise blog platform with tutorials,tips & tricks,and seo articles to help you manage or build your own cloud enterprise website',
-    // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: 'AOM Cloud Enterprise Blog', // This allows an alternative site description for meta data for pages.                                                                                                   
+    siteDescriptionMeta: 'A cloud enterprise blog platform with tutorials,tips & tricks,and seo articles to help you manage or build your own cloud enterprise website', // This allows an alternative site description for meta data for pages.
 
     shareImage: `/images/banner.jpg`, // fallback image for meta data. Will be used, when no post/tag/author image specified. 1200x1200 is recommended
     shareImageWidth: 1000, // Change to the width of your default share image.
     shareImageHeight: 523, // Change to the height of your default share image.
 
-    shortTitle: `AOMCloud`, // Used for App manifest e.g.
+    shortTitle: 'AOMCloud', // Used for App manifest e.g.
 
  Mobile Home Screen
-    siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest.
+    siteIcon: 'favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest.
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest.
     themeColor: `#15171A`, // Used for Offline Manifest
 }
