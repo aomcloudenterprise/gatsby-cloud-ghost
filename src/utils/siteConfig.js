f877ecae81a76abb15c59baf8b3e25fd1364f940
danklut <aomcloudenterprise@outlook.com>
2019-02-09 01:31:44 -0800

    Update siteConfig.js

--- src/utils/siteConfig.js
+++ src/utils/siteConfig.js
@@ -13,7 +13,7 @@ module.exports = {
     shortTitle: 'AOMCloud', // Used for App manifest e.g.
 
  Mobile Home Screen
-    siteIcon: 'favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest.
-    backgroundColor: `#e9e9e9`, // Used for Offline Manifest.
+    siteIcon: 'favicon.png', // Logo in /static dir used for SEO, RSS, and App manifest.
+    backgroundColor: '#e9e9e9', // Used for Offline Manifest.
     themeColor: `#15171A`, // Used for Offline Manifest
 }
